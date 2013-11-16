de2dbd34a154710c0ac53d8e106e2698d9912fca
Clauz <clauz@ninux.org>
2013-11-16 18:34:06 +0100

    make crawl.sh more robust

--- crawl.sh
+++ crawl.sh
@@ -2,7 +2,19 @@
 
 NINUXOO_DIR=/opt/ninuxoo/ninuu
 
+for p in $(ps ax -opid,cmd | grep crawler.py | awk '{print $1}'); do 
+	kill $p 
+	sleep 5
+	kill -9 $p
+done
+
+ls -lh /tmp/ninuxoo.log 1>&2
+file /tmp/ninuxoo.log 1>&2
+curdate=$(date +"%s")
+cp /tmp/ninuxoo.log /tmp/ninuxoo${curdate}.log
+echo "cp /tmp/ninuxoo.log /tmp/ninuxoo${curdate}.log" 1>&2
+
 cd $NINUXOO_DIR
-${NINUXOO_DIR}/getprefixesfromroutingtable.sh > ${NINUXOO_DIR}/IpPrefixes.txt && time python crawler.py; 
+${NINUXOO_DIR}/getprefixesfromroutingtable.sh > ${NINUXOO_DIR}/IpPrefixes.txt && time python2.7 crawler.py 
 echo $(date) "---------";  
 
